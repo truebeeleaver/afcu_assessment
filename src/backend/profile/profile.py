b40ab2f3d5c65acc16b8c747a260a2bc89f2f67c
Eric Stewart <eastewar@fastmail.us>
2020-05-29 12:41:11 -0400

    Validate requests using refactored error handling

--- src/backend/profile/profile.py
+++ src/backend/profile/profile.py
@@ -1,42 +1,74 @@
 import logging
+import json
 
-from backend.server.request import HttpRequest
+import backend.server.request as request
+import backend.server.util as sutil
+import backend.profile.util as putil
+
 from backend.server.response import HttpResponse
 
 logger = logging.getLogger(__name__)
 
+
 def handleLogin(req, profiles):
     logger.info(f"Processing {req.verb} on login resource")
-    if req.verb != "POST":
-        # Invalid verb
-        return HttpResponse(400)
+
+    putil.assertValidVerb(req, {request.HTTP_POST})
+    putil.assertContentType(req, "application/json")
+
+    content = putil.getJsonContent(req)
+
+    if "username" not in content or "password" not in content:
+        # Missing required fields
+        raise sutil.HttpException(400, "Expected username and password fields")
+    username = content["username"]
+    password = content["password"]
+    
     resp = HttpResponse(201)
 
-    # TODO login
+    # TODO handle login
 
     return resp
 
+
 def handleSignup(req, profiles):
     logger.info(f"Processing {req.verb} on signup resource")
-    if req.verb != "POST":
-        # Invalid verb
-        return HttpResponse(400)
+
+    putil.assertValidVerb(req, {request.HTTP_POST})
+    putil.assertContentType(req, "application/json")
+
+    content = putil.getJsonContent(req)
+    if (
+        "username" not in content
+        or "password" not in content
+        or "phone" not in content
+    ):
+        # Missing required fields
+        raise sutil.HttpException(
+            400, "Expected username, password, and phone fields"
+        )
+    username = content["username"]
+    password = content["password"]
+    phone = content["phone"]
+
     resp = HttpResponse(201)
 
     # TODO signup
 
     return resp
 
+
 def handleProfile(req, profiles):
     logger.info(f"Processing {req.verb} on profile resource")
-    if req.verb != "GET":
-        # Invalid verb
-        return HttpResponse(400)
+
+    putil.assertValidVerb(req, {request.HTTP_GET})
+
     resp = HttpResponse(200)
 
-    # TODO profile 
+    # TODO profile
 
     return resp
+
 
 class ProfileManager:
     def __init__(self):
---
+++ src/backend/profile/util.py
@@ -0,0 +1,33 @@
+import json
+
+import backend.server.request as request
+import backend.server.util as sutil
+from backend.server.response import HttpResponse
+
+
+def assertValidVerb(req, verbs):
+    if req.verb not in verbs:
+        # Invalid verb
+        raise sutil.HttpException(
+            400,
+            f"Unacceptable verb {req.verb}, valid verbs for this resource are {verbs}",
+        )
+    return None
+
+
+def assertContentType(req, content_type):
+    if (
+        "Content-Type" not in req.headers
+        or req.headers["Content-Type"] != content_type
+    ):
+        raise sutil.HttpException(
+            400, f"Unacceptable content type. Expected {content_type}"
+        )
+    return None
+
+
+def getJsonContent(req):
+    try:
+        return json.loads(str(req.content, "utf8"))
+    except json.decoder.JSONDecodeError as e:
+        raise sutil.HttpException(400, f"Invalid JSON: {str(e)}")
