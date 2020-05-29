01a209a6db5de11c7c0ee219e072a33c845b9885
Eric Stewart <eastewar@fastmail.us>
2020-05-29 11:42:25 -0400

    Fix worker thread cleanup
    Don't always log normal termination, always clean up connection

--- src/backend/server/connection.py
+++ src/backend/server/connection.py
@@ -21,14 +21,13 @@ def handleConnection(server, conn, addr):
             
             logger.info(f"Writing response {str(resp)}")
             resp.writeResponse(buffered_conn)
+        logger.info(f"Connection with {addr} terminated normally")
     except socket.timeout:
         logger.info(f"Connection timed out with {addr}")
     except Exception as e:
         # swallowing exceptions is Not Good in general but worker threads should terminate on any errors anyway, and we want to log instead of print to console
         # being exception safe is up to the service code
         logger.exception(e)
-        # NB: conn is likely always valid here--but doc was unclear without more digging
-        if conn:
-            conn.shutdown(socket.SHUT_RDWR)
-            conn.close()
-    logger.info(f"Connection with {addr} terminated normally")
+    finally:
+        conn.shutdown(socket.SHUT_RDWR)
+        conn.close()
